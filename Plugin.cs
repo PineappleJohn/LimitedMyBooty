db2e2ea9c0d64cd633d547d5c7058714efcfae66
John <forddarwin6@gmail.com>
2024-11-06 19:46:22 -0600

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Newtilla;
+
+namespace LimitedMyBooty
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start()
+        {
+            Newtilla.Newtilla.AddGameMode("GHOSTTAG", "GHOSTTAG", "GHOST", false);
+            Newtilla.Newtilla.AddGameMode("AMBUSH", "AMBUSH", "AMBUSH", false);
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace LimitedMyBooty
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.john.gorillatag.limitedmybooty";
+        public const string Name = "LimitedMyBooty";
+        public const string Version = "1.0.0";
+    }
+}
